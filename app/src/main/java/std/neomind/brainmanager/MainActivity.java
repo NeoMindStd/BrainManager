5bd29ed75bc6da16353523901b026d000365dddf
도우진 <dwj1996@naver.com>
2019-06-03 01:31:05 +0900

    Change data register routine
    설명 또는 키워드나 카테고리의 이름이 공백인 경우 등록이 불가능하게 변경

--- app/src/main/java/std/neomind/brainmanager/MainActivity.java
+++ app/src/main/java/std/neomind/brainmanager/MainActivity.java
@@ -370,21 +370,27 @@ public class MainActivity extends AppCompatActivity
                                     Category resultCategory = new Category.Builder().
                                             setName(editText.getText().toString())
                                             .build();
-                                    mBrainDBHandler.addCategory(resultCategory);
-                                    try {
-                                        resultCategory = mBrainDBHandler.findLastCategory();
-                                        for(int i = 1; i < mCategories.size(); i++) {
-                                            if(i ==  mCategories.size()-1) {
-                                                mCategories.add(resultCategory);
-                                                break;
-                                            } else if(resultCategory.name.
-                                                    compareToIgnoreCase(mCategories.get(i).name) < 0) {
-                                                mCategories.add(i, resultCategory);
-                                                break;
+                                    if(resultCategory.name.isEmpty()) {
+                                        Toast.makeText(this, getString(R.string.Global_exceptionESCategory), Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        mBrainDBHandler.addCategory(resultCategory);
+                                        try {
+                                            resultCategory = mBrainDBHandler.findLastCategory();
+                                            for (int i = 1; i < mCategories.size(); i++) {
+                                                if (i == mCategories.size() - 1) {
+                                                    mCategories.add(resultCategory);
+                                                    break;
+                                                } else if (resultCategory.name.
+                                                        compareToIgnoreCase(mCategories.get(i).name) < 0) {
+                                                    mCategories.add(i, resultCategory);
+                                                    break;
+                                                }
                                             }
+                                            initSpinner();
+                                        } catch (BrainDBHandler.NoMatchingDataException e) {
+                                            e.printStackTrace();
                                         }
-                                        initSpinner();
-                                    } catch (BrainDBHandler.NoMatchingDataException e) { e.printStackTrace(); }
+                                    }
                                 })
                         .setNeutralButton(getString(R.string.Global_negative), null)
                         .show();
